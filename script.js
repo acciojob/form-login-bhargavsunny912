dbbb9b7b37dbd883b7c469db57707f3aaa6def09
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-11-04 12:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+let first=document.forms[0].fname;
+let last=document.forms[0].lname;
+document.getElementById("form1").addEventListener('submit',(e)=>{
+	e.preventDefault();
+	if(first.value.trim()!=="" && last.value.trim()!==""){
+		alert(`${first.value.trim()} ${last.value.trim()}`);
+	}
+	form1.reset();
+})
+function getFormvalue(e) {
 
 }
