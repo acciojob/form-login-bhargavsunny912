683945188fa7f7645103019f7f356f9e1644ccd8
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-11-04 12:44:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let last=document.forms[0].lname;
 document.getElementById("form1").addEventListener('submit',(e)=>{
 	e.preventDefault();
 	if(first.value.trim()!=="" && last.value.trim()!==""){
-		alert(`${first.value.trim()} ${last.value.trim()}`);
+		getFormvalue(first,last);
 	}
 	form1.reset();
 })
-function getFormvalue(e) {
-
+function getFormvalue(first,last) {
+	alert(`${first.value.trim()} ${last.value.trim()}`);
 }
